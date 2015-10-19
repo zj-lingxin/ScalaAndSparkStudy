58fb0be568b084a217e2831d3e6503e9e964f3a6
zj-lingxin <690094095@qq.com>
2015-10-19 09:29:34 +0800

    增加截取字符串的正则表达式

--- src/main/scala/myscala/scalaexercises/regular_expressions/RegexTest.scala
+++ src/main/scala/myscala/scalaexercises/regular_expressions/RegexTest.scala
@@ -6,10 +6,16 @@ class RegexTest {
   @Test
   def regex1() = {
     //业务：用正则表达式将"[1112212,23232]"替换成 1112212,23232 ，即去掉",[,]
-    val regex = """[\[\"\]]""".r
-    val oldStr = "\"[1112212,23232]\""
+    var regex = """[\[\"\]]""".r
+    var oldStr = "\"[1112212,23232]\""
     println("替换前的文本:" + oldStr)
-    val newStr = regex.replaceAllIn(oldStr, "")
+    var newStr = regex.replaceAllIn(oldStr, "")
     println("替换后的文本:" + newStr)
+
+    //将"2015/2/12"截取出"2015/2"
+    regex = """\d{4}/\d{1,2}""".r
+    val date = "2015/12/2"
+    val yearMonth = regex.findFirstIn(date)
+    println(regex) //"2015/12
   }
 }
